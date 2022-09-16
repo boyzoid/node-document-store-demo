00d340ca3059f796adf0e73365390feec027389a
Scott Stroz <scott.stroz@gmail.com>
2022-09-16 07:44:11 -0400

    Cleaned up code; fixed typos

--- README.md
+++ README.md
@@ -4,19 +4,19 @@ A demo for using Node.js to access a MySQL Document Store
 
 ## Setup
 
-This setup assumes you already have [Node.js](https://nodejs.org/en/download/) and [MySQl Shell](https://dev.mysql.com/downloads/shell/) installed and have access to a MySQL database.
+This setup assumes you already have [Node.js](https://nodejs.org/en/download/) and [MySQL Shell](https://dev.mysql.com/downloads/shell/) installed and have access to a MySQL database.
 
-* Open MySQL Shell and connect to your MySQL instance using the follwoing command: `\c {user}:{password}@{host}:33060`
+* Open MySQL Shell and connect to your MySQL instance using the following command: `\c {user}:{password}@{host}:33060`
 
   * Where `{user}` is the username, `{password}` is the password, and `{host}` is the server domain name or IP address of your MySQL instance.
 * In MySQL Shell, run the command `session.createSchema('node_demo')` to create the new schema.
-* In MySQL Shell, run the follwoing command: `util.importJson( '/absolute/path/to/project/data/scores.json', {schema: 'node_demo', collection: 'scores'})`
+* In MySQL Shell, run the following command: `util.importJson( '/absolute/path/to/project/data/scores.json', {schema: 'node_demo', collection: 'scores'})`
 
-  * If the process runs successfully, you will see output simialr to this:
+  * If the process runs successfully, you will see output similar to this:
     `Processed 12.65 MB in 17477 documents in 4.7405 sec (3.69K documents/s)  Total successfully imported documents 17477 (3.69K documents/s)`
-* In the project root directory, copy the .env_template file to .env and fill in the values for the port Node will listen on and the database information.
+* In the project root directory, copy the `.env_template` file to `.env` and fill in the values for the port Node will listen on and the database information.
 * From a command prompt, run `npm install` in the project root directory.
-* From a command prompt, run `node src` in the project root directory.'
+* From a command prompt, run `node src` in the project root directory.
 * In a browser window, or at tool such as Postman, make a `GET` request to [http://localhost:3001/](http://localhost:3001/)
 
   * You should see the following result: `{"message":"Node Demo main endpoint"}`
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// ./src/index.js
-
 // importing the dependencies
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -19,7 +17,7 @@ const mysqlx = require('@mysql/xdevapi')
 // Database/Schema Name
 const databaseName = 'node_demo'
 const collectionName = 'scores'
-const connectionUrl =  `mysqlx://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${databaseName}`
+const connectionUrl = `mysqlx://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${databaseName}`
 
 // Connection URL
 const pool = mysqlx.getClient(connectionUrl, {
@@ -44,329 +42,345 @@ app.get('/', (req, res) => {
 });
 
 // /list endpoint
-app.get('/list/', async (req, res ) =>{
+app.get('/list/', async (req, res) => {
     const scores = await listAllScores()
-    let msg = { count: scores.length, scores: scores }
-    res.send( msg )
-    
+    let msg = {count: scores.length, scores: scores}
+    res.send(msg)
+
 })
 // find() demo
-const listAllScores = async () =>{
+const listAllScores = async () => {
     let scores = []
     const session = await pool.getSession()
     const db = session.getSchema(databaseName)
     const collection = db.getCollection(collectionName)
     await collection.find()
-        .execute((score) => {
-            scores.push(score)
-        });
-    
-    session.close()
-    return scores
-}
-// /list with limit
-app.get('/list/:limit', async (req, res ) =>{
-    const scores = await limitAllScores(req.params.limit)
-    let msg = { count: scores.length, scores: scores }
-    res.send( msg )
-    
-})
-// Showing find() with limit()
-const limitAllScores = async ( limit ) =>{
-    let scores = []
-    const session = await pool.getSession()
-    const db = session.getSchema(databaseName)
-    const collection = db.getCollection(collectionName)
-    await collection.find()
-        .limit(limit)
-        .execute((score) => {
-            scores.push(score)
-        });
+                    .execute((score) => {
+                        scores.push(score)
+                    });
 
     session.close()
     return scores
 }
+
+// /list with limit
+app.get('/list/:limit', async (req, res) => {
+    const scores = await limitAllScores(req.params.limit)
+    let msg = {count: scores.length, scores: scores}
+    res.send(msg)
+
+})
+
+// Showing find() with limit()
+const limitAllScores = async (limit) => {
+    let scores = []
+    const session = await pool.getSession()
+    const db = session.getSchema(databaseName)
+    const collection = db.getCollection(collectionName)
+    await collection.find()
+                    .limit(limit)
+                    .execute((score) => {
+                        scores.push(score)
+                    });
+
+    session.close()
+    return scores
+}
+
 // /bestScores endpoint
-app.get('/bestScores/:limit?', async (req, res) =>{
+app.get('/bestScores/:limit?', async (req, res) => {
     let limit = req.params.limit ? req.params.limit : defaultResultLength
     const scores = await getBestScores(limit)
-    let msg = { count: scores.length, scores: scores }
-    res.send( msg )
+    let msg = {count: scores.length, scores: scores}
+    res.send(msg)
 })
+
 // showing find() with fields() and sort()
-const getBestScores = async ( limit ) =>{
+const getBestScores = async (limit) => {
     let scores = []
     const session = await pool.getSession()
     const db = session.getSchema(databaseName)
     const collection = db.getCollection(collectionName)
     await collection.find()
-        .fields(['firstName', 'lastName', 'score', 'date', 'course.name as courseName'])
-        .sort(['score asc', 'date desc'])
-        .limit( limit )
-        .execute((score) => {
-            scores.push(score)
-        });
+                    .fields(['firstName', 'lastName', 'score', 'date', 'course.name as courseName'])
+                    .sort(['score asc', 'date desc'])
+                    .limit(limit)
+                    .execute((score) => {
+                        scores.push(score)
+                    });
     session.close()
     return scores
 }
 
-app.get('/getRoundsUnderPar', async (req, res) =>{
+app.get('/getRoundsUnderPar', async (req, res) => {
     const scores = await getRoundsUnderPar()
-    let msg = { count: scores.length, scores: scores }
-    res.send( msg )
+    let msg = {count: scores.length, scores: scores}
+    res.send(msg)
 })
+
 // Showing find() with numeric comparison
-const getRoundsUnderPar = async () =>{
+const getRoundsUnderPar = async () => {
     let scores = []
     const session = await pool.getSession()
     const db = session.getSchema(databaseName)
     const collection = db.getCollection(collectionName)
     await collection.find("score < course.par")
-        .fields(['firstName', 'lastName', 'score', 'date', 'course.name as courseName'])
-        .execute((score) => {
-            scores.push( score )
-        })
+                    .fields(['firstName', 'lastName', 'score', 'date', 'course.name as courseName'])
+                    .execute((score) => {
+                        scores.push(score)
+                    })
     session.close()
     return scores
 }
 
-app.get('/getByScore/:score?', async (req, res) =>{
+app.get('/getByScore/:score?', async (req, res) => {
     let score = req.params.score ? req.params.score : 36
     const scores = await getByScore(parseInt(score))
-    let msg = { count: scores.length, scores: scores }
-    res.send( msg )
+    let msg = {count: scores.length, scores: scores}
+    res.send(msg)
 })
+
 //show find() with bind()
-const getByScore = async ( score ) =>{
+const getByScore = async (score) => {
     let scores = []
     const session = await pool.getSession()
     const db = session.getSchema(databaseName)
     const collection = db.getCollection(collectionName)
     await collection.find("score = :score")
-        .bind( 'score', score)
-        .fields(['concat( firstName, " ", lastName) as golfer', 'score', 'date', 'course.name as courseName'])
-        .sort(['date desc'])
-        .execute((score) => {
-            scores.push(score)
-        });
+                    .bind('score', score)
+                    .fields(['concat(firstName, " ", lastName) as golfer', 'score', 'date', 'course.name as courseName'])
+                    .sort(['date desc'])
+                    .execute((score) => {
+                        scores.push(score)
+                    });
     session.close()
     return scores
 }
 
-app.get('/getByGolfer/:lastName?', async (req, res) =>{
+app.get('/getByGolfer/:lastName?', async (req, res) => {
     let str = req.params.lastName || ''
     const scores = await getByGolfer(str)
-    let msg = { count: scores.length, scores: scores }
-    res.send( msg )
+    let msg = {count: scores.length, scores: scores}
+    res.send(msg)
 })
+
 // showing find() using 'like'
-const getByGolfer = async ( lastName ) =>{
+const getByGolfer = async (lastName) => {
     let scores = []
     const session = await pool.getSession()
     const db = session.getSchema(databaseName)
     const collection = db.getCollection(collectionName)
     await collection.find("lower(lastName) like :lastName")
-        .bind('lastName', lastName.toLowerCase()+'%')
-        .sort(['lastName', 'firstName'])
-        .execute((score) => {
-            scores.push(score)
-        });
+                    .bind('lastName', lastName.toLowerCase() + '%')
+                    .sort(['lastName', 'firstName'])
+                    .execute((score) => {
+                        scores.push(score)
+                    });
     session.close()
     return scores
 }
 
-app.get('/getAverageScorePerGolfer/:year?', async (req, res) =>{
-    const scores = await getAverageScorePerGolfer( req.params.year )
-    let msg = { count: scores.length, scores: scores }
-    res.send( msg )
-    
+app.get('/getAverageScorePerGolfer/:year?', async (req, res) => {
+    const scores = await getAverageScorePerGolfer(req.params.year)
+    let msg = {count: scores.length, scores: scores}
+    res.send(msg)
+
 })
+
 // show find() using logic for year, aggregate data in fields(), and groupBy()
-const getAverageScorePerGolfer = async ( year ) =>{
+const getAverageScorePerGolfer = async (year) => {
     let scores = []
     const session = await pool.getSession()
     const db = session.getSchema(databaseName)
     const collection = db.getCollection(collectionName)
-    await collection.find( year ? "year(date) = " + year : 'date is not null')
-        .fields(['lastName', 'firstName', 'round(avg(score), 2) as avg', 'count(score) as numberOfRounds'])
-        .sort([ 'lastName', 'firstName'])
-        .groupBy(['lastName', 'firstName'])
-        .execute((score) => {
-            scores.push( score )
-        })
+    await collection.find(year ? "year(date) = " + year : 'date is not null')
+                    .fields(['lastName', 'firstName', 'round(avg(score), 2) as avg', 'count(score) as numberOfRounds'])
+                    .sort(['lastName', 'firstName'])
+                    .groupBy(['lastName', 'firstName'])
+                    .execute((score) => {
+                        scores.push(score)
+                    })
     session.close()
     return scores
 }
 
-app.get('/getCourseScoringData', async (req, res) =>{
+app.get('/getCourseScoringData', async (req, res) => {
     const scores = await getCourseScoringData()
-    let msg = { count: scores.length, scores: scores }
-    res.send( msg )
+    let msg = {count: scores.length, scores: scores}
+    res.send(msg)
 })
+
 // show find with groupBy() on child property.
-const getCourseScoringData = async () =>{
+const getCourseScoringData = async () => {
     let scores = []
     const session = await pool.getSession()
     const db = session.getSchema(databaseName)
     const collection = db.getCollection(collectionName)
     await collection.find()
-        .fields(['course.name as courseName',
-                    'round(avg(score), 2)  as avg',
-                    'cast(min(score) as unsigned) as lowestScore',
-                    'cast(max(score) as unsigned) as highestScore',
-                    'count(score) as numberOfRounds'])
-        .groupBy(['course.name'])
-        .sort('course.name desc')
-        .execute((score) => {
-            scores.push( score )
-        })
+                    .fields([
+                                'course.name as courseName',
+                                'round(avg(score), 2)  as avg',
+                                'cast(min(score) as unsigned) as lowestScore',
+                                'cast(max(score) as unsigned) as highestScore',
+                                'count(score) as numberOfRounds'
+                            ])
+                    .groupBy(['course.name'])
+                    .sort('course.name desc')
+                    .execute((score) => {
+                        scores.push(score)
+                    })
     session.close()
     return scores
 }
 
-app.get('/getHolesInOne', async (req, res) =>{
-    const aces = await getHolesInOne(  )
-    let msg = { count: aces.length, aces:aces }
-    res.send( msg )
+app.get('/getHolesInOne', async (req, res) => {
+    const aces = await getHolesInOne()
+    let msg = {count: aces.length, aces: aces}
+    res.send(msg)
 })
+
 // use SQL with path operator, JSON_ARRAYAGG(), JSON_OBJECT(), and JSON_TABLE()
-const getHolesInOne = async () =>{
+const getHolesInOne = async () => {
     let aces = []
     const session = await pool.getSession()
-    
+
     const sql = `SELECT JSON_OBJECT(
-            'firstName', doc ->> '$.firstName',
-            'lastName', doc ->> '$.lastName',
-            'date', doc ->> '$.date',
-            'courseName', doc->> '$.course.name',
-            'holes', JSON_ARRAYAGG(
-                JSON_OBJECT(
-                'holeNumber', holeScores.number,
-                            'par', holeScores.par
-                        )
-                    )
-        )
-        FROM scores,
-        JSON_TABLE(
-        doc, '$.holeScores[*]'
-            COLUMNS (
-            score INT PATH '$.score',
-            number INt PATH '$.number',
-            par INT PATH '$.par'
-        )
-        ) holeScores
-        WHERE holeScores.score = 1
-        GROUP by doc ->> '$.lastName', doc ->> '$.firstName'
-        ORDER by doc ->> '$.date' DESC`
-        
-    const query = await session.sql( sql )
-    await query.execute( (ace) => {
+                                'firstName', doc ->> '$.firstName',
+                                'lastName', doc ->> '$.lastName',
+                                'date', doc ->> '$.date',
+                                'courseName', doc ->> '$.course.name',
+                                'holes', JSON_ARRAYAGG(
+                                        JSON_OBJECT(
+                                                'holeNumber', holeScores.number,
+                                                'par', holeScores.par
+                                            )
+                                    )
+                            )
+                 FROM scores,
+                      JSON_TABLE(
+                              doc, '$.holeScores[*]'
+                              COLUMNS (
+                                  score INT PATH '$.score',
+                                  number INt PATH '$.number',
+                                  par INT PATH '$.par'
+                                  )
+                          ) holeScores
+                 WHERE holeScores.score = 1
+                 GROUP by doc ->> '$.lastName', doc ->> '$.firstName'
+                 ORDER by doc ->> '$.date' DESC`
+
+    const query = await session.sql(sql)
+    await query.execute((ace) => {
         aces.push(ace)
     })
     session.close()
     return aces
 }
 
-app.get('/getAggregateCourseScore', async (req, res) =>{
-    const courses = await getAggregateCourseScore(  )
-    let msg = { courses:courses }
-    res.send( msg )
-    
+app.get('/getAggregateCourseScore', async (req, res) => {
+    const courses = await getAggregateCourseScore()
+    let msg = {courses: courses}
+    res.send(msg)
+
 })
+
 // Show SQL with using JSON data in a common table expression
 const getAggregateCourseScore = async () => {
     let courses = []
     const session = await pool.getSession()
-    
+
     const sql = `
         WITH aggScores AS
-            (SELECT doc ->> '$.course.name' course,
-                MIN(score) minScore,
-                MAX(score) maxScore,
-                number
-            FROM scores,
-            JSON_TABLE( doc, '$.holeScores[*]'
-                COLUMNS ( score INT PATH '$.score',
-                number INT PATH '$.number' )) AS scores
-            GROUP BY course, number
-            ORDER BY course, number)
-        SELECT JSON_OBJECT( 'courseName', course, 'bestScore',sum( minScore ) )
+                 (SELECT doc ->> '$.course.name' course,
+                         MIN(score)              minScore,
+                         MAX(score)              maxScore,
+                         number
+                  FROM scores,
+                       JSON_TABLE(doc, '$.holeScores[*]'
+                                  COLUMNS (score INT PATH '$.score',
+                                      number INT PATH '$.number')) AS scores
+                  GROUP BY course, number
+                  ORDER BY course, number)
+        SELECT JSON_OBJECT('courseName', course, 'bestScore', sum(minScore))
         FROM aggScores
         GROUP BY course
         ORDER BY course;`
-        
-    const query = await session.sql( sql )
-    await query.execute( (course) => {
+
+    const query = await session.sql(sql)
+    await query.execute((course) => {
         courses.push(course)
     })
     session.close()
     return courses
 }
 
-app.post('/score', async function(req, res, response){
-    const success = await addScore( req.body );
-    let msg = { success: success }
-    res.send( msg )
+app.post('/score', async function (req, res, response) {
+    const success = await addScore(req.body);
+    let msg = {success: success}
+    res.send(msg)
 });
+
 //Add a score to DB
-const addScore = async ( score ) =>{
+const addScore = async (score) => {
     let success = true;
     const session = await pool.getSession()
     const db = session.getSchema(databaseName)
     const collection = db.getCollection(collectionName)
-    try{
-        await collection.add( score ).execute()
+    try {
+        await collection.add(score).execute()
     }
-    catch ( e ) {
+    catch (e) {
         success = false
     }
     session.close()
     return success
 }
 
-app.post('/holeScores', async function(req, res, response){
-    const success = await addHoleScores( req.body );
-    let msg = { success: success }
-    res.send( msg )
+app.post('/holeScores', async function (req, res, response) {
+    const success = await addHoleScores(req.body);
+    let msg = {success: success}
+    res.send(msg)
 });
+
 //Add a score to DB
-const addHoleScores = async ( data ) =>{
+const addHoleScores = async (data) => {
     let success = true;
     const session = await pool.getSession()
     const db = session.getSchema(databaseName)
     const collection = db.getCollection(collectionName)
-    try{
-        await collection.modify( "_id = :id" ).set("holeSores", data.holeScores).bind("id", data._id).execute()
+    try {
+        await collection.modify("_id = :id").set("holeSores", data.holeScores).bind("id", data._id).execute()
     }
-    catch ( e ) {
+    catch (e) {
         success = false
     }
     session.close()
     return success
 }
 
-app.get('/removeScore/:id?', async function(req, res, response){
+app.get('/removeScore/:id?', async function (req, res, response) {
     let msg = {}
-    if( req.params.id ){
-        let success = await removeScore( req.params.id )
+    if (req.params.id) {
+        let success = await removeScore(req.params.id)
         msg.success = success
     }
-    else{
-        msg.error =  "Please provide a valid id."
+    else {
+        msg.error = "Please provide a valid id."
     }
-    
-    res.send( msg )
+
+    res.send(msg)
 });
+
 //Add a score to DB
-const removeScore = async ( id ) =>{
+const removeScore = async (id) => {
     let success = true;
     const session = await pool.getSession()
     const db = session.getSchema(databaseName)
     const collection = db.getCollection(collectionName)
-    try{
-        await collection.remove( "_id = :id" ).bind("id", id).execute()
+    try {
+        await collection.remove("_id = :id").bind("id", id).execute()
     }
-    catch ( e ) {
+    catch (e) {
         success = false
     }
     session.close()
